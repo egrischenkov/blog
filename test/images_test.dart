599c97863c1e50c24fe6c39a4dfa24c33fe4581b
Egor Grischenkov <66774252+egrischenkov@users.noreply.github.com>
2023-12-21 19:50:01 +0300

    chore(blog): setup structure of directories and got BLoC dependencies

--- test/images_test.dart
+++ test/images_test.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:blog/assets/res/resources.dart';
+import 'package:blog/assets//resources.dart';
 
 void main() {
   test('images assets test', () {});
